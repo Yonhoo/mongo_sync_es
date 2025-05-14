4d09e0ff1cc6bcc74a4f7413cf7595c9c8bfde7c
YonHoo <cookies_yon@163.com>
2025-05-14 20:27:59 +0800

    [feat]: update docker file

--- docker/plugin/build.sh
+++ docker/plugin/build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-plugin=$(<.plugin)
+# Set the plugin name
+plugin="collection_process"
 
-# # Build a docker image
-docker build --build-arg PLUGIN="$plugin" \
+# Update the .plugin file with the plugin name
+echo ${plugin} > .plugin
+
+# Build a docker image
+docker build --build-arg PLUGIN="${plugin}" \
        -f ./Dockerfile -t monstache-plugin ../..
 
 # Start a container from the newly built docker image
-docker run --rm -d monstache-plugin tail -f /app/docker/plugin/$plugin.go
+docker run --rm -d monstache-plugin tail -f /app/docker/plugin/${plugin}.go
 
 # Get the container id of the last created container
 CONTAINER_ID=$(docker ps -l -q)
-
 
 # If the folder docker-build exists locally
 if [ -d docker-build ] ; then
@@ -21,7 +24,9 @@ fi
 
 mkdir docker-build
 
-docker cp "$CONTAINER_ID":/app/docker/plugin/$plugin.so ./docker-build/$plugin.so
+docker cp "$CONTAINER_ID":/app/docker/plugin/${plugin}.so ./docker-build/${plugin}.so
 
 # Stop the container (it'll be removed automatically once stopped, as we used `--rm`)
 docker stop "$CONTAINER_ID"
+
+echo "Plugin built successfully! The compiled .so file is in ./docker-build/${plugin}.so"
